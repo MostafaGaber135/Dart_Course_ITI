caee0bb5d9e1980393e4afd3254a85d999e1b589
Mostafa Gaber Ahmed Hussien <119273107+MostafaGaber135@users.noreply.github.com>
2025-04-16 13:40:54 +0200

    Add files via upload

---
+++ Day1/test.dart
@@ -0,0 +1,131 @@
+void main() {
+  //   object
+  //   Object x = 22;
+  //   print(x);
+  //   Object bol = true;
+  //   print(bol);
+  //   -------------------------------------------------------
+  //   num
+  //   num y = 2;
+  //   print(y);
+  //   num z = 22.22;
+  //   print(z);
+  //   --------------------------------------------------------
+  //   int
+  //   int x = 22;
+  //   print(x);
+  // ---------------------------------------------------------
+  //   double
+  //   double y = 22.22;
+  //   print(y);
+  //   ---------------------------------------------------------
+  //   String
+  //   String name = "Hello World";
+  //   print(name);
+  //   ---------------------------------------------------------
+  //   bool
+  //   bool isTrue = true;
+  //   print(isTrue);
+  //   ---------------------------------------------------------
+  //   var , dynamic , final , const , Late
+  //   var is a keyword that tells the compiler to infer the type of the variable based on the value assigned to it but can not be change latter.
+  //   var x = 22;
+  //   print(x);
+  //   x = "Hello";
+  //   print(x);
+  //   -----------------------------------------------------------------------
+  //   dynamic is a keyword that tells the compiler that the variable can hold any type of value and can be changed later.
+  //   dynamic x = 22;
+  //   print(x);
+  //   x = "Hello";
+  //   print(x);
+  //   -----------------------------------------------------------------------
+  //   final is a keyword that tells the compiler that the variable is a constant and can not be changed later.
+  //   final x = 22;
+  //   print(x);
+  //   // x = "Hello"; // error
+  //   -----------------------------------------------------------------------
+  //   const is a keyword that tells the compiler that the variable is a constant and can not be changed later.
+  //   const x = 22;
+  //   print(x);
+  //   // x = "Hello"; // error
+  //   -----------------------------------------------------------------------
+  //   late is a keyword that tells the compiler that the variable is a constant and can not be changed later.
+  //   late int x;
+  //   x = 22;
+  //   print(x);
+  //   -----------------------------------------------------------------------
+  //   late String name; //  nullable variable
+  //   name = "Ahmed"; //  // can not set in runtime but can set in compile time
+  //   print(name);
+  //   -----------------------------------------------------------------------
+  //   // Collections
+  //   // List
+  //   List<String> names = ["Ahmed", "Ali", "Mohamed"]; //  List of Strings
+  //   print(names[0]); //  print the first element of the list
+  //   print(names.length); //  print the length of the list
+  //   -----------------------------------------------------------------------
+  //   // Map
+  //   Map<String, String> person = {"name": "Ahmed", "age": "22"}; //  Map of Strings
+  //   print(person["name"]); //  print the value of the key "name"
+  //   print(person.length); //  print the length of the map
+  //   -----------------------------------------------------------------------
+  //   Set
+  //   Set<String> names2 = {"Ahmed", "Ali", "Mohamed", "Ahmed"}; //  Set of Strings
+  //   print(names2); //  print the set
+  //   print(names2.length); //  print the length of the set
+  //   -----------------------------------------------------------------------
+  //   Operators
+  //   // Arithmetic Operators
+  //   + , - , * , / , %, ~/
+  //   int a = 10;
+  //   int b = 5;
+  //   print(a + b); //  print the sum of a and b
+  //   print(a - b); //  print the difference of a and b
+  //   print(a * b); //  print the product of a and b
+  //   print(a / b); //  print the quotient of a and b
+  //   print(a % b); //  print the remainder of a and b
+  //   print(a ~/ b); //  print the integer division of a and b
+  //   -----------------------------------------------------------------------
+  //   // Logical Operators
+  //   && , || , !, == , != , > , < , >= , <=
+  //   bool x = true;
+  //   bool y = false;
+  //   print(x && y); //  print the logical and of x and y
+  //   print(x || y); //  print the logical or of x and y
+  //   print(!x); //  print the logical not of x
+  //   print(x == y); //  print the logical equality of x and y
+  //   print(x != y); //  print the logical inequality of x and y
+  //   print(x > y); //  print the logical greater than of x and y
+  //   print(x < y); //  print the logical less than of x and y
+  //   print(x >= y); //  print the logical greater than or equal to of x and y
+  //   print(x <= y); //  print the logical less than or equal to of x and y
+  //   -----------------------------------------------------------------------
+  //   // Conditions
+  //   if , else , else if
+  //   int x = 10;
+  //   if (x > 0) {
+  //     print("x is positive");
+  //   } else if (x < 0) {
+  //     print("x is negative");
+  //   } else {
+  //     print("x is zero");
+  //   }
+  //   -----------------------------------------------------------------------
+  //   // Loops
+  //   for , while , do while
+  //   for (int i = 0; i < 10; i++) {
+  //     print(i);
+  //   }
+  //   int i = 0;
+  //   while (i < 10) {
+  //     print(i);
+  //     i++;
+  //   }
+  //   int i = 0;
+  //   do {
+  //     print(i);
+  //     i++;
+  //   } while (i < 10);
+  //   -----------------------------------------------------------------------
+}
